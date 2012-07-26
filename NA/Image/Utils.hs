b6ba110f1b9d846a0ccb9eba071abfde98c673da
Abby Henríquez Tejera <paradoja@gmail.com>
2012-07-27 00:21:30 +0100

    Added toHSV conversion.

--- NA/Image/Utils.hs
+++ NA/Image/Utils.hs
@@ -4,6 +4,7 @@ module NA.Image.Utils
     , channelToWord8
     , normalise
     , duplicate
+    , toHSV
     )
 where
 
@@ -43,3 +44,30 @@ duplicateChannel c1 c2 = runSTUArray $ do
                            return dup
     where ((h0, w0), (h1, w1)) = bounds c1
           width  = (w1-w0+1)*2 + 4 + w0 - 1
+
+toHSV :: Image Word8 -> (Channel Word8, Channel Word8, Channel Word8)
+toHSV img@(Image red green blue) = let channels = [red, green, blue]
+                                       cMin :: Word8
+                                       cMin     = minimum $ map (cfold1 min) channels
+                                       cMaxs    = map (cfold1 max) channels
+                                   in hsvMaxFirst cMaxs cMin img 0
+
+hsvMaxFirst :: [Word8] -> Word8 -> Image Word8 -> Word8 -> (Channel Word8, Channel Word8, Channel Word8)
+hsvMaxFirst list@[x,y,z] cMin img@(Image cx cy cz) disp
+       | maximum list == x = finishHSV x cMin img disp
+       | otherwise         = hsvMaxFirst [y,z,x] cMin (Image cy cz cx) (disp+85)
+hsvMaxFirst _ _ _ _ = error "should receive a 3 element list"
+
+finishHSV :: Word8 -> Word8 -> Image Word8 -> Word8 -> (Channel Word8, Channel Word8, Channel Word8)
+finishHSV cMax cMin (Image cx cy cz) disp =
+    let sizes = bounds cx
+        diff = cMax - cMin
+        v = array sizes $ zip (range sizes) $ repeat cMax
+        s = array sizes $ zip (range sizes) $ repeat diff
+        h = runSTUArray $ do
+              new <- newArray sizes 0
+              forM_ (reverse $ range sizes) $ \(j,i) -> do
+                            let val = 43 *((cy!(j,i)) - (cz!(j,i))) `div` diff + disp
+                            writeArray new (j,i ) val
+              return new
+        in (v, s, h)
--- README.md
+++ README.md
@@ -2,9 +2,3 @@ numerical-analysis-haskell
 ==========================
 
 A couple of Numerical Analysis methoods, with examples.
-
-Requires [Codec.BMP](http://hackage.haskell.org/packages/archive/bmp/latest/doc/html/Codec-BMP.html):
-
-````
-cabal install bmp
-````
